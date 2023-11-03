1cc5faa5c4eebfce718903df1e485831f49465f7
Lukas Cizek <globus303@gmail.com>
2023-11-03 18:18:18 +0100

    Fix event creation bug (#11)

--- models/event.go
+++ models/event.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type Event struct {
-	ID          int64     `json:"-"`
+	ID          uint16    `json:"-"`
 	Public_ID   string    `json:"id" example:"pwnrxtbi9z0v"`
 	Name        string    `json:"name" example:"Basketball Match at Park"`
 	Sport       string    `json:"sport" example:"Basketball"`
 	Date        time.Time `json:"date" example:"2023-07-10"`
 	Location    string    `json:"location" example:"Central Park"`
-	Price       float64   `json:"price" example:"0.00"`
+	Price       uint16    `json:"price" example:"123"`
 	Description string    `json:"description" example:"Example Description"`
 	Level       string    `json:"level" example:"Any"`
+	Created_At  time.Time `json:"createdAt" example:"2023-11-03T10:15:30Z"`
 }
