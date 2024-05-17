5d0a62d0de12f908f8738506e117e69bc9607545
Lukas Cizek <globus303@gmail.com>
2024-05-17 13:05:33 +0200

    Fix casing on message struct (#22)

--- models/message.go
+++ models/message.go
@@ -7,13 +7,13 @@ import (
 type EmailRequest struct {
 	ID           uint16     `json:"id" example:"1"`
 	Text         string     `json:"text" example:"I would like to join your event."`
-	EventID      string     `json:"event_id" example:"pwnrxtbi9z0v"`
-	EventOwnerID string     `json:"event_owner_id" example:"pwnrxtbi9z0v"`
-	RequesterID  string     `json:"requester_id" example:"pwnrxtbi9z0v"`
+	EventID      string     `json:"eventId" example:"pwnrxtbi9z0v"`
+	EventOwnerID string     `json:"eventOwnerId" example:"pwnrxtbi9z0v"`
+	RequesterID  string     `json:"requesterId" example:"pwnrxtbi9z0v"`
 	Approved     *bool      `json:"approved" example:"false"`
-	ApprovedAt   *time.Time `json:"approved_at,omitempty" example:"2023-11-03T10:15:30Z"`
-	CreatedAt    time.Time  `json:"created_at" example:"2023-11-03T10:15:30Z"`
-	UpdatedAt    time.Time  `json:"updated_at" example:"2023-11-03T10:15:30Z"`
+	ApprovedAt   *time.Time `json:"approvedAt,omitempty" example:"2023-11-03T10:15:30Z"`
+	CreatedAt    time.Time  `json:"createdAt" example:"2023-11-03T10:15:30Z"`
+	UpdatedAt    time.Time  `json:"updatedAt" example:"2023-11-03T10:15:30Z"`
 }
 
 type EmailRequestInput struct {
